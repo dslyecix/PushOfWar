0f03500b5a2c67c7e57b18fc4a451a28dd9cd2e8
dslyecix <31083701+dslyecix@users.noreply.github.com>
2018-10-24 14:35:52 -0400

    hmmm

---
+++ PushOfWarProject/Assets/KinematicCharacterController/Core/Editor/ComponentEditors.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace KinematicCharacterController
+{
+    [CustomEditor(typeof(Rigidbody))]
+    public class RigidbodyEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            Rigidbody r = (target as Rigidbody);
+            if (r.gameObject.GetComponent<KinematicCharacterMotor>() || r.gameObject.GetComponent<PhysicsMover>())
+            {
+                GUI.enabled = false;
+                DrawDefaultInspector();
+                GUI.enabled = true;
+            }
+            else
+            {
+                DrawDefaultInspector();
+            }
+        }
+    }
+
+    [CustomEditor(typeof(CapsuleCollider))]
+    public class CapsuleColliderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            CapsuleCollider c = (target as CapsuleCollider);
+            if (c.gameObject.GetComponent<KinematicCharacterMotor>())
+            {
+                GUI.enabled = false;
+                DrawDefaultInspector();
+                GUI.enabled = true;
+            }
+            else
+            {
+                DrawDefaultInspector();
+            }
+        }
+    }
+}
